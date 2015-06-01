8a6f5597b867581969b3109c769dd78c9ce9379f
Arnold Krille <arnold@arnoldarts.de>
2015-06-01 23:28:25 +0200

    try with a plug-file

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,12 +1,11 @@
 
-import os
 from errbot.backends.test import testbot, push_message, pop_message
 
 # import pytest
 
 
 class TestChatbot(object):
-    extra_plugins_dir = '../watering'
+    extra_plugins_dir = '.'
 
     def test_hello(self, testbot):
         push_message('!hello')
