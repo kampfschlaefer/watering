8d547e00a87c1fe6a9c330bfbae4a81893dfa884
Arnold Krille <arnold@arnoldarts.de>
2015-06-14 19:39:27 +0200

    fix flake8

--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -1,14 +1,19 @@
 
 from errbot.backends.test import testbot, push_message, pop_message
+import pytest
 
 
+__extra_fixtures = testbot
+
+
+@pytest.mark.usefixtures('testbot')
 class TestChatbot(object):
     extra_plugin_dir = '.'
 
-    def test_help(self, testbot):
+    def test_help(self):
         push_message('!help WaterBot')
         assert 'Say hello' in pop_message()
 
-    def test_hello(self, testbot):
+    def test_hello(self):
         push_message('!hello')
         assert 'Hello' in pop_message()
